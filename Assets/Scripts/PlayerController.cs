efd21dd7da94d05f92a9a93f149d2d47d92e93e5
Izaac <56174514+izaac13@users.noreply.github.com>
2019-10-25 12:56:45 -0700

    Finished step seven so that I could make stuff collide

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Playercontroller : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,7 @@ public class Playercontroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       // Move the vehicle forward
+       transform.Translate(Vector3.forward * Time.deltaTime * 20);
     }
 }
